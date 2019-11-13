f362f2227064175162f86a97daa9c5355d45a9ab
Jared <jared@digitalnutt.com>
2019-11-13 10:31:31 -0800

    working on homepage

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,45 +1,100 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import CardList from '../components/CardList'
-import Card from '../components/Card'
+// import CardList from '../components/CardList'
+// import Card from '../components/Card'
 import Helmet from 'react-helmet'
 import Container from '../components/Container'
-import Pagination from '../components/Pagination'
+// import Pagination from '../components/Pagination'
 import SEO from '../components/SEO'
 import config from '../utils/siteConfig'
+import HomeHero from '../components/HomeHero'
+
+import styled from 'styled-components'
+const Article = styled.article`
+  display: flex;
+  flex-direction: ${props => (props.direction ? props.direction : null)};
+`
 
 const Index = ({ data, pageContext }) => {
-  const posts = data.allContentfulPost.edges
-  const featuredPost = posts[0].node
-  const { currentPage } = pageContext
-  const isFirstPage = currentPage === 1
+  // const posts = data.allContentfulPost.edges
+  // const featuredPost = posts[0].node
+  // const { currentPage } = pageContext
+  // const isFirstPage = currentPage === 1
+
+  const groupList = [
+    'Trauma Education',
+    'Trauma Process Group',
+    'Co-Dependency Group',
+    'Helping Woman Recover',
+    'Helping Men Recover',
+    'Substance Use Disorder (SUD) support group',
+    'Family Education Group',
+    'Mindfulness Relapse Prevention',
+    'Early Recovery Skills',
+    'Moral Reconation Therapy (MRT)',
+    'Anger Management',
+  ]
 
   return (
     <Layout>
       <SEO />
-      {!isFirstPage && (
-        <Helmet>
-          <title>{`${config.siteTitle} - Page ${currentPage}`}</title>
-        </Helmet>
-      )}
+      <Helmet>
+        <title>{config.siteTitle}</title>
+      </Helmet>
       <Container>
-        {isFirstPage ? (
-          <CardList>
-            <Card {...featuredPost} featured />
-            {posts.slice(1).map(({ node: post }) => (
-              <Card key={post.id} {...post} />
+        <HomeHero>Words</HomeHero>
+        <Article direction="column">
+          <h1>Services</h1>
+          <ul>
+            <li>Individual Therapy</li>
+            <li>Group Therapy</li>
+            <li>Co-Occuring Disorder Treatment</li>
+            <li>Trauma Informed Treatment</li>
+          </ul>
+        </Article>
+        <Article direction="column">
+          <h1>Groups</h1>
+          <ul>
+            {groupList.map(group => (
+              <li>{group}</li>
             ))}
-          </CardList>
-        ) : (
-          <CardList>
-            {posts.map(({ node: post }) => (
-              <Card key={post.id} {...post} />
-            ))}
-          </CardList>
-        )}
+          </ul>
+        </Article>
+        <Article direction="column">
+          <h1>Where We Are</h1>
+          <h2>Office Hours</h2>
+          <p>9:00 AM &mdash; 5:00 PM</p>
+          <h2>Address</h2>
+          <address>
+            <p>850 E Franklin Rd. Ste 405</p>
+            <p>Meridian, Idaho 83642</p>
+          </address>
+        </Article>
       </Container>
-      <Pagination context={pageContext} />
+
+      {/* {!isFirstPage && ( */}
+      {/*   <Helmet> */}
+      {/*     <title>{`${config.siteTitle} - Page ${currentPage}`}</title> */}
+      {/*   </Helmet> */}
+      {/* )} */}
+      {/* <Container> */}
+      {/*   {isFirstPage ? ( */}
+      {/*     <CardList> */}
+      {/*       <Card {...featuredPost} featured /> */}
+      {/*       {posts.slice(1).map(({ node: post }) => ( */}
+      {/*         <Card key={post.id} {...post} /> */}
+      {/*       ))} */}
+      {/*     </CardList> */}
+      {/*   ) : ( */}
+      {/*     <CardList> */}
+      {/*       {posts.map(({ node: post }) => ( */}
+      {/*         <Card key={post.id} {...post} /> */}
+      {/*       ))} */}
+      {/*     </CardList> */}
+      {/*   )} */}
+      {/* </Container> */}
+      {/* <Pagination context={pageContext} /> */}
     </Layout>
   )
 }
